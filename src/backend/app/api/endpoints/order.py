eaed94c55b58c297477f6d64369f31299fc0f94a
Ковалев Егор <pulseefast@gmail.com>
2024-08-01 11:24:41 +0700

    routs

--- src/backend/app/api/endpoints/order.py
+++ src/backend/app/api/endpoints/order.py
@@ -4,3 +4,10 @@ order_router = APIRouter(
     prefix="/react-order",
     tags=["order"],
 )
+
+@order_router.get("/products")
+async def get_products():
+    
+@order_router.get("/products/{id}")
+async def get_product(id: int):
+    
